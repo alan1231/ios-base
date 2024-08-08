c1fd6191e71d3e3842b1f485294c4eff0b3ccd3c
Ed <ed@riversense.tw>
2024-08-08 10:19:02 +0800

    Initial Commit

---
+++ baseView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  baseView
+//
+//  Created by mmm on 2024/8/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
